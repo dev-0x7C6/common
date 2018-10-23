639341b329bc4d630a3b1ec57bd8a8c5b7531864
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2018-10-23 22:51:33 +0200

    Working on raii-value-assigment and support for reference types.

--- raii/raii-value-assigment.hpp
+++ raii/raii-value-assigment.hpp
@@ -5,21 +5,20 @@
 template <typename type, auto initial_state, auto final_state>
 class raii_value_assigment {
 public:
-	raii_value_assigment()
-			: m_value(initial_state) {}
+	raii_value_assigment(type &&initial = {initial_state})
+			: m_value(std::forward<type>(initial)) {
+		m_value = initial_state;
+	}
 
 	raii_value_assigment(const raii_value_assigment &) = delete;
 	raii_value_assigment &operator=(const raii_value_assigment &) = delete;
 
 	raii_value_assigment(raii_value_assigment &&rhs) noexcept
-			: m_value(std::move(rhs.m_value)) {
+			: m_value(rhs.m_value) {
 		rhs.m_moved = true;
 	}
 
-	raii_value_assigment &operator=(raii_value_assigment &&rhs) noexcept {
-		rhs.m_value = std::move(rhs);
-		rhs.m_moved = true;
-	}
+	raii_value_assigment &operator=(raii_value_assigment &&rhs) noexcept = delete;
 
 	~raii_value_assigment() {
 		if (!m_moved)
