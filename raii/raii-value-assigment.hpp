6ed1be24da70a267f59d8b73a6490421c4c7603c
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2018-10-23 22:13:17 +0200

    Added move assigment operator and move constructor for raii_value_assigment.

--- raii/raii-value-assigment.hpp
+++ raii/raii-value-assigment.hpp
@@ -1,18 +1,36 @@
 #pragma once
 
+#include <utility>
+
 template <typename type, auto initial_state, auto final_state>
 class raii_value_assigment {
 public:
 	raii_value_assigment()
-			: m_value(initial_state) {
+			: m_value(initial_state) {}
+
+	raii_value_assigment(const raii_value_assigment &) = delete;
+	raii_value_assigment &operator=(const raii_value_assigment &) = delete;
+
+	raii_value_assigment(raii_value_assigment &&rhs) noexcept
+			: m_value(std::move(rhs.m_value)) {
+		rhs.m_moved = true;
+	}
+
+	raii_value_assigment &operator=(raii_value_assigment &&rhs) noexcept {
+		rhs.m_value = std::move(rhs);
+		rhs.m_moved = true;
 	}
 
 	~raii_value_assigment() {
-		m_value = final_state;
+		if (!m_moved)
+			m_value = final_state;
 	}
 
-	auto &value() noexcept { return m_value; }
+	constexpr operator type &() noexcept {
+		return m_value;
+	}
 
 private:
 	type m_value;
+	bool m_moved{false};
 };
